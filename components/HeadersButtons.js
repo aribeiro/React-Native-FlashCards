07468ea38c00910b2ee6a82cda119f90732edad3
Alfredo Ribeiro <me@alfredoribeiro.com>
2019-02-09 17:42:54 -0200

    [Feat] HeadersButtons to display + button in the navigation menu, used to add New Deck

---
+++ components/HeadersButtons.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { HeaderButtons, HeaderButton } from 'react-navigation-header-buttons';
+
+// define IconComponent, color, sizes and OverflowIcon in one place
+const MaterialHeaderButton = props => (
+  <HeaderButton {...props} IconComponent={MaterialIcons} iconSize={40} color="white" />
+);
+
+export const MaterialHeaderButtons = props => {
+  return (
+    <HeaderButtons
+      HeaderButtonComponent={MaterialHeaderButton}
+      OverflowIcon={<MaterialIcons name="more-vert" size={23} color="white" />}
+      {...props}
+    />
+  );
+};
+
+export const Item = HeaderButtons.Item;

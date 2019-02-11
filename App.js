dd1ae2deae2260fbefe8541065e91123ad7be429
Alfredo Ribeiro <me@alfredoribeiro.com>
2019-02-11 08:02:49 -0200

    [FEATURE] Adding Redux to manage States

--- App.js
+++ App.js
@@ -1,38 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
-import { createMaterialTopTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation'
-import { Constants } from 'expo'
-import DeckList from './components/DeckList'
-import DeckForm from './components/DeckForm'
+import {StyleSheet, Text, View, StatusBar, Platform} from 'react-native';
+import {
+  createMaterialTopTabNavigator,
+  createStackNavigator,
+  createAppContainer,
+} from 'react-navigation';
+import {Constants} from 'expo';
+import DeckList from './components/DeckList';
+import DeckForm from './components/DeckForm';
 
-import CardForm from  './components/CardForm'
-import DeckCards from './components/DeckCards'
-import Quiz from './components/Quiz'
-// --------------------------------------------------------//
-// import { createStore } from 'redux'
-// import { Provider } from 'react-redux'
-// import reducer from './reducers'
+import CardForm from './components/CardForm';
+import DeckCards from './components/DeckCards';
+import Quiz from './components/Quiz';
 
-const StackNavigator = createStackNavigator(
-  {
-    Home: DeckList,
-    AddDeck: DeckForm,
-    Deck: DeckCards,
-    Quiz: Quiz,
-    AddCard: CardForm
-  }
-);
-const Stack = createAppContainer(StackNavigator)
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+import middleware from './middleware'
 
+const store = createStore(reducers, middleware)
+
+const StackNavigator = createStackNavigator({
+  Home: DeckList,
+  AddDeck: DeckForm,
+  Deck: DeckCards,
+  Quiz: Quiz,
+  AddCard: CardForm,
+});
+const Stack = createAppContainer(StackNavigator);
 
 export default class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
+    <Provider store={store}>
         {/* <FlashCardStatusBar backgroundColor={styles.statusBar.backgroundColor} barStyle="light-content" /> */}
         <Stack />
-      </React.Fragment>
+    </Provider>
     );
   }
 }
-

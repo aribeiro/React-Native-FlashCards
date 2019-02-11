39d0df6219d28bc29250c33b7e913d2954ac71dd
Alfredo Ribeiro <me@alfredoribeiro.com>
2019-02-11 08:05:46 -0200

    [FEAT] Adding redux to DeckForm

--- components/DeckForm.js
+++ components/DeckForm.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
-import  Button from './Button'
-import * as API from '../api/api'
-import { submitDeck } from '../api/api'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import Button from './Button';
+
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {handleSaveDeckTitle} from '../actions/decks';
 
 class DeckForm extends Component {
   static navigationOptions = {
@@ -15,47 +23,58 @@ class DeckForm extends Component {
     headerTitleStyle: {
       fontWeight: 'bold',
     },
-  }
-  state = { name: '', created: false, error: null }
+  };
+  state = {name: '', created: false, error: null};
 
-  changeName = (name) => {
-    this.setState({ name })
-  }
+  changeName = name => {
+    this.setState({name});
+  };
 
-	onPressButton = () => {
-    const { name } = this.state
-    if(this.state.name){
+  onPressButton = () => {
+    const {name} = this.state;
+    if (this.state.name) {
       // Generating KEY for AsyncStorage
-      const key = (new Date().valueOf());
-
-      API.saveDeckTitle({title: name, key: key})
-
+      const key = new Date().valueOf();
+      this.props.handleSaveDeckTitle(name, key);
       this.props.navigation.navigate('Deck', {deckId: key});
-
-		} else {
-    	this.setState({ error: 'you need to add a name for your New Deck' })
-		}
-	}
+    } else {
+      this.setState({error: 'you need to add a name for your New Deck'});
+    }
+  };
 
   render() {
-    const { name, error } = this.state;
+    const {name, error} = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Add New Deck</Text>
         <TextInput
-          placeholder='New deck name'
+          placeholder="New deck name"
           style={styles.textInput}
           value={name}
-          onChangeText={name => this.changeName(name) }
+          onChangeText={name => this.changeName(name)}
         />
         {error && <Text style={styles.error}>{error}</Text>}
-        <Button onPress={this.onPressButton} text='CREATE' />
+        <Button onPress={this.onPressButton} text="CREATE" />
       </View>
     );
   }
 }
 
-export default DeckForm
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      handleSaveDeckTitle,
+    },
+    dispatch,
+  );
+}
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(DeckForm);
+
+// export default DeckForm
 
 const styles = StyleSheet.create({
   container: {
@@ -63,7 +82,7 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginRight: 10,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   header: {
     alignItems: 'center',
@@ -77,12 +96,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     borderColor: '#000',
     borderStyle: 'solid',
-    borderBottomWidth: 1
+    borderBottomWidth: 1,
   },
   btnText: {
-    color: '#fff'
+    color: '#fff',
   },
   error: {
-    color: 'red'
-  }
+    color: 'red',
+  },
 });
